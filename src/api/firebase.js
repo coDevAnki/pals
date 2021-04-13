8bf697660c090d650494178db5a1cc96492e5453
unknown <impratsbanerjee@gmail.com>
2021-04-13 13:13:28 +0530

    set up env variables

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDihLp58FOkhEBwvgG-xg4i13OAv2bey9Y",
-  authDomain: "pals-5ae48.firebaseapp.com",
-  projectId: "pals-5ae48",
-  storageBucket: "pals-5ae48.appspot.com",
-  messagingSenderId: "505437498863",
-  appId: "1:505437498863:web:0a1a2daf144abfa8e71a1a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 
 export const storage = firebase.storage();
